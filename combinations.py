c00b1ab8a158016acc76f5f93aba6987584c7305
Chetan Sharma <46220343+Chetan8076@users.noreply.github.com>
2020-10-22 19:34:27 +0530

    updated combinations.py with permutations also given in itertools

--- combinations.py
+++ combinations.py
@@ -1,5 +1,8 @@
-from itertools import combinations
+from itertools import combinations,permutations
 
-arr=[1,2,3,4,]
+arr=[1,2,3,4]
 l=list(combinations(arr,3))
 print(l)
+
+lp = list(permutations(arr,3))
+print(lp)
